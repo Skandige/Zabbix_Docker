fa89b113419e46e6503b7859c29a50ea79d17da1
Skandige <skandige@yandex.ru>
2022-05-27 12:16:29 +0300

    Zabbix Docker v0.3

--- zabbix_bak.sh
+++ zabbix_bak.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 getDate() {
-	date '+%d-%m-%Y_%H-%M-%S'
+	date '+%d.%m.%Y_%H:%M:%S'
 }
 
 a="/home/zabbix/zabbix.log"
 
 
 echo "Начало архивирования базы $(getDate)" >> $a
-echo "Остановка контейнера Zabbix" >> $a
+echo "Остановка контейнера $(getDate)" >> $a
 docker-compose down 
-echo "Архивирование базы Zabbix" >> $a
+echo "Архивирование базы Zabbix $(getDate)" >> $a
 tar -cvf ./backups/zabbix_mysql_$(getDate).tar ./mysql
-echo "Запуск контейнера Zabbix" >> $a
+echo "Запуск контейнера Zabbix $(getDate)" >> $a
 docker-compose up -d 
-echo "Удаление стаых бекапов" >> $a
+echo "Удаление стаых бекапов $(getDate)" >> $a
 /usr/bin/find /home/zabbix/backups -mtime +4 -type f -exec rm -rf {} \;
-echo "Смена привелегий каталога" >> $a
+echo "Смена привелегий каталога $(getDate)" >> $a
 chown -R  support:support /home/zabbix/backups/ 
 echo "_______________________________" >> $a
